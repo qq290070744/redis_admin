1dc5176a8a4a39fad23db1421fcfb5c6e7db3690
jiangwenhui <290070744@qq.com>
2024-03-11 16:24:56 +0800

    2024-03-11

--- loginfo/models.py
+++ loginfo/models.py
@@ -9,10 +9,10 @@ from datetime import datetime
 
 
 class OperationInfo(models.Model):
-    username = models.CharField(max_length=20, verbose_name=u"用户名")
-    server = models.CharField(max_length=20, verbose_name=u"redis名")
+    username = models.CharField(max_length=255, verbose_name=u"用户名")
+    server = models.CharField(max_length=255, verbose_name=u"redis名")
     db = models.CharField(max_length=20, verbose_name=u"db名")
-    key = models.CharField(max_length=120, db_index=True, verbose_name=u"key")
+    key = models.CharField(max_length=255, db_index=True, verbose_name=u"key")
     old_value = models.TextField(verbose_name=u"old value")
     value = models.TextField(null=True, blank=True, verbose_name=u"value")
     type = models.CharField(max_length=10, verbose_name=u"类型")
