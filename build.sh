6dc4e20d0f83e71d0239b579b5a0aae3c32bfd25
jiangwenhui <290070744@qq.com>
2024-03-18 14:12:31 +0800

    2024-03-18

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
-tag=202403015.5
+tag=202403018
 docker build -t registry.cn-shanghai.aliyuncs.com/jwh/redis_admin:$tag .
 docker push registry.cn-shanghai.aliyuncs.com/jwh/redis_admin:$tag
 echo $tag
--- redis_admin/settings.py
+++ redis_admin/settings.py
@@ -16,7 +16,7 @@ from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+# BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
@@ -197,6 +197,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static')
 ]
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static/admin')
 
 LOGIN_URL = '/login/'
 LOGOUT_URL = '/logout/'
